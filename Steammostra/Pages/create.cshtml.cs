9f368d0988529739dff860405acda516956eb442
GRAZIELE MOTA DA SILVA <021.898684@senaicimatec.edu.br>
2024-09-16 18:00:13 -0300

    julia

--- Steammostra/Pages/create.cshtml.cs
+++ Steammostra/Pages/create.cshtml.cs
@@ -2,18 +2,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using Steammostra.Models;
 
-
 namespace Steammostra.Pages
 {
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+
         public CreateModel(ApplicationDbContext context)
         {
             _context = context;
         }
+
         [BindProperty]
         public Logins Logins { get; set; }
+
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
@@ -21,9 +23,12 @@ namespace Steammostra.Pages
                 return Page();
             }
 
+            // Adicionar o login ao banco de dados
             _context.Logins.Add(Logins);
             _context.SaveChanges();
-            return RedirectToPage("./home");
+
+            // Redirecionar para a página inicial
+            return RedirectToPage("/home"); // Assegure-se de que "Index" é a página inicial que você deseja redirecionar
         }
     }
 }
