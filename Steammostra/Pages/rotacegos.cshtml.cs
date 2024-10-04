24c91b4ff90324692a28818793a685d3e2144cf2
Seu Nome <silvajulia3012.com>
2024-10-04 01:09:33 -0300

    noite

---
+++ Steammostra/Pages/rotacegos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Steammostra.Pages
+{
+    public class rotacegosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
