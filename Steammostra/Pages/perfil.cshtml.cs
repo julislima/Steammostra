05255ccf7a5944f0630eeae6dda3ad2e2bf65f48
MATHEUS SANTOS PEREIRA <021.898669@senaicimatec.edu.br>
2024-11-08 16:40:48 -0300

    perfil

---
+++ Steammostra/Pages/perfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Steammostra.Pages
+{
+    public class perfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
