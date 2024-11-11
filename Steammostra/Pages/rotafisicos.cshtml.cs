dd3d992255564cf9b1b6e9d9c3dbfb1fc195ad15
JULIA SILVA LIMA <021.898697@senaicimatec.edu.br>
2024-11-11 17:41:29 -0300

    falta as imagens

---
+++ Steammostra/Pages/rotafisicos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Steammostra.Pages
+{
+    public class rotafisicosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
