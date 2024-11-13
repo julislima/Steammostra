32221c5e57ce4f2c6ab48ed8606b13f6b8c1c0e0
Seu Nome <silvajulia3012.com>
2024-11-13 11:07:57 -0300

    ACABOU

---
+++ Steammostra/Pages/visu3fisicos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Steammostra.Pages
+{
+    public class visu3fisicosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
