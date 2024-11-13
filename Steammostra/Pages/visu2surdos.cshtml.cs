ed5438b83063e0a488958f788ab6195b26fc9c7a
Seu Nome <silvajulia3012.com>
2024-11-13 01:40:54 -0300

    faltam só detalhes

---
+++ Steammostra/Pages/visu2surdos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Steammostra.Pages
+{
+    public class visu2surgosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
